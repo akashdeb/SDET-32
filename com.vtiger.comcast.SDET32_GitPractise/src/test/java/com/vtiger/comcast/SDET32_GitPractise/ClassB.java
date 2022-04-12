b52f9cf834716116710ee193e8f039953173afe9
DELL_P <DELL_P@PANKAJ>
2022-04-12 12:24:05 +0530

    Commit2

---
+++ com.vtiger.comcast.SDET32_GitPractise/src/test/java/com/vtiger/comcast/SDET32_GitPractise/ClassB.java
@@ -0,0 +1,12 @@
+package com.vtiger.comcast.SDET32_GitPractise;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	
+	@Test
+	public void demoTest2(){
+		System.out.println("Second Commit");
+	}
+
+}
